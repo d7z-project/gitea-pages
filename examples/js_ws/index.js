8d7c7d21106b9c6d338aaf05b95b0d10e548c02d
dragon <ExplodingFKL@gmail.com>
2026-01-29 14:15:08 +0800

    docs: simplify js_ws example

--- examples/js_ws/index.js
+++ examples/js_ws/index.js
@@ -2,17 +2,6 @@
     let ws = websocket();
     while (true) {
         let data = await ws.readText();
-        switch (data) {
-            case "exit":
-                return
-            case "panic":
-                throw Error("错误");
-            case "date":
-                await ws.writeText(new Date().toJSON())
-                break
-            default:
-                await ws.writeText("收到信息:" + data)
-                break;
-        }
+        await ws.writeText("ECHO: " + data)
     }
 })()
